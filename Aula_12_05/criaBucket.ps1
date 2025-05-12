2f20528a727409542dcbafdeb482d70dde4e6c2c
cauanstocco <cauanstocco@gmail.com>
2025-05-12 10:41:24 -0300

    Atividade

---
+++ Aula_12_05/criaBucket.ps1
@@ -0,0 +1,13 @@
+﻿# Script PowerShell para criar bucket S3 com LocalStack
+
+
+$Endpoint = "http://localhost:4566"
+$Bucket = Read-Host -Prompt "Insira o nome do bucket:"
+
+
+# 1. Criar um bucket
+Write-Host "Criando o bucket $Bucket..."
+aws s3 mb "s3://$Bucket" --endpoint-url $Endpoint --profile "localstack"
+
+# 2. Lista o bucket criado
+aws s3 ls "s3://$Bucket" --endpoint-url $Endpoint --profile "localstack"
---
+++ Aula_12_05/localStackStart.ps1
@@ -0,0 +1,50 @@
+# Defina seu perfil AWS
+$awsProfileName = "localstack"
+$awsAccessKey = "test"
+$awsSecretKey = "test"
+$awsRegion = "us-east-1"
+
+# Caminhos para os arquivos de configuração
+$awsDir = "$HOME\.aws"
+$credentialsFile = "$awsDir\credentials"
+$configFile = "$awsDir\config"
+
+# Criação da pasta .aws se não existir
+if (-Not (Test-Path $awsDir)) {
+    New-Item -ItemType Directory -Path $awsDir | Out-Null
+}
+
+# Criar ou sobrescrever o arquivo credentials
+Set-Content -Path $credentialsFile -Value @"
+[$awsProfileName]
+aws_access_key_id = $awsAccessKey
+aws_secret_access_key = $awsSecretKey
+"@
+
+# Criar ou sobrescrever o arquivo config
+Set-Content -Path $configFile -Value @"
+[profile $awsProfileName]
+region = $awsRegion
+output = json
+"@
+
+Write-Host "Arquivos de perfil AWS criados em $awsDir."
+
+# Iniciar o Docker se necessário
+if ((Get-Service -Name "com.docker.service" -ErrorAction SilentlyContinue).Status -ne 'Running') {
+    Write-Host "Iniciando o serviço Docker..."
+    Start-Service -Name "com.docker.service"
+    Start-Sleep -Seconds 10
+} else {
+    Write-Host "Docker já está em execução."
+}
+
+# Verificar se o LocalStack container está rodando
+$localstackRunning = docker ps --filter "name=localstack_main" --format "{{.Names}}"
+
+if (-not $localstackRunning) {
+    Write-Host "Iniciando o container LocalStack..."
+    docker run --rm -d -p 4566:4566 -p 4571:4571 --name localstack_main localstack/localstack
+} else {
+    Write-Host "LocalStack já está rodando como container: $localstackRunning"
+}
